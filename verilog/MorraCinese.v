39e0af2a24fae00bcd1d950bdd5dec6380d21684
Oddraa <leonardomilone55@gmail.com>
2023-12-21 18:35:26 +0100

    Initial Commit
    Giusto per inizializzare qualcosa

---
+++ verilog/MorraCinese.v
@@ -0,0 +1,10 @@
+module moduleName (
+    input wire [1:0] PRIMO;
+    input wire [1:0] SECONDO;
+    input wire [0:0] INIZIA;
+
+    output reg [1:0] MANCHE;
+    output reg [1:0] PARTITA;
+);
+    
+endmodule
