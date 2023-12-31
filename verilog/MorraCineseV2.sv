0a5afbdd11b104be434fd683aecaa8ac1adc0113
Oddraa <leonardomilone55@gmail.com>
2023-12-31 19:32:20 +0100

    Update MorraCineseV2.sv

--- verilog/MorraCineseV2.sv
+++ verilog/MorraCineseV2.sv
@@ -1,20 +1,21 @@
 module moduleName (
-    // Segnale di clock
-    input wire clk,
+        // Segnale di clock
+        input wire clk,
 
-    // Ingressi del Circuito
-    input wire [1:0] PRIMO;
-    input wire [1:0] SECONDO,
-    input wire [0:0] INIZIA = 1'b1,  // Impostato a 1 di default
+        // Ingressi del Circuito
+        input wire [1:0] PRIMO;
+        input wire [1:0] SECONDO,
+        input wire [0:0] INIZIA = 1'b1,  // Impostato a 1 di default
 
-    // Uscite del Circuito
-    output wire [1:0] MANCHE,
-    output wire [1:0] PARTITA,
+        // Uscite del Circuito
+        output wire [1:0] MANCHE,
+        output wire [1:0] PARTITA,
 
-    // Registri per la memorizzazione delle vittorie
-    output reg [4:0] nPartite = 5'b00000;
-    output reg [4:0] winPrimo = 5'b00000;
-    output reg [4:0] winSecondo = 5'b00000;
+        // Registri per la memorizzazione delle vittorie
+        output reg [4:0] nPartite = 5'b00000,
+        output reg [4:0] winPrimo = 5'b00000,
+        output reg [4:0] winSecondo = 5'b00000
+    );      
 
     // se il valore di INIZIA = 1 allora entra nelle casistiche assegnando il valore a nPartite
     // appena verifica una delle casistiche, imposta il valore di INIZIA = 0 cosi da non eseguire piu il blocco
