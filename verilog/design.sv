dab90b7614438f0aa4afb260034cf23503dee409
Oddraa <leonardomilone55@gmail.com>
2024-01-04 16:19:48 +0100

    Update design.sv

--- verilog/design.sv
+++ verilog/design.sv
@@ -1,4 +1,5 @@
 module moduleName (
+    
         // Segnale di clock
         input wire clk,
 
@@ -22,6 +23,8 @@ module moduleName (
     reg [4:0] partiteVintePrimo;
     reg [4:0] partiteVinteSecondo;
 
+    reg [0:0] vincitore; // valore a 1bit che oscilla tra 0 e 1 a seconda del vincitore tra PRIMO e SECONDA
+
     // se il valore di INIZIA = 1 allora entra nelle casistiche assegnando il valore a nPartite
     // appena verifica una delle casistiche, imposta il valore di INIZIA = 0 cosi da non eseguire piu il blocco
     always @(posedge clk) begin
@@ -35,107 +38,231 @@ module moduleName (
             INIZIA <= 1'b0;
         end
         else if (INIZIA == 1'b0) begin
+
+            
+
             case ({PRIMO, SECONDO})
 
                 4'b00 00: // manche non valida
 
-                4'b00 01: 
+                4'b00 01: // considerata vittoria a tavolino
 
-                    contatoreManche += 5'b00001;
-                    winSecondo += 5'b00001;
-                    mossaVincitore = {SECONDO};
+                    if (vincitore == 1'b1 && mossaVincitore == {SECONDO}) begin
+                        MANCHE = 1'b00;
+                    end
+                    else if (vincitore == 1'b0 && mossaVincitore == {PRIMO}) begin
+                        MANCHE = 1'b00;
+                    end
+                    else begin
+                        contatoreManche += 5'b00001;
+                        winSecondo += 5'b00001;
+                        // mossaVincitore = {SECONDO};
+                        vincitore = 1;
+                    end
 
                 4'b00 10:
 
-                    contatoreManche += 5'b00001;
-                    winSecondo += 5'b00001;
-                    mossaVincitore = {SECONDO};
+                    if (vincitore == 1'b1 && mossaVincitore == {SECONDO}) begin
+                        MANCHE = 1'b00;
+                    end
+                    else if (vincitore == 1'b0 && mossaVincitore == {PRIMO}) begin
+                        MANCHE = 1'b00;
+                    end
+                    else begin
+                        contatoreManche += 5'b00001;
+                        winSecondo += 5'b00001;
+                        // mossaVincitore = {SECONDO};
+                    end
 
                 4'b00 11:
 
-                    contatoreManche += 5'b00001;
-                    winSecondo += 5'b00001;
-                    mossaVincitore = {SECONDO};
+                    if (vincitore == 1'b1 && mossaVincitore == {SECONDO}) begin
+                        MANCHE = 1'b00;
+                    end
+                    else if (vincitore == 1'b0 && mossaVincitore == {PRIMO}) begin
+                        MANCHE = 1'b00;
+                    end
+                    else begin
+                        contatoreManche += 5'b00001;
+                        winSecondo += 5'b00001;
+                        // mossaVincitore = {SECONDO};
+                    end
 
                 /////////////////////////////////////////
 
                 4'b01 00:
-
-                    contatoreManche += 5'b00001;
-                    winPrimo += 5'b00001;
-                    mossaVincitore = {PRIMO};
+                    if (vincitore == 1'b1 && mossaVincitore == {SECONDO}) begin
+                        MANCHE = 1'b00;
+                    end
+                    else if (vincitore == 1'b0 && mossaVincitore == {PRIMO}) begin
+                        MANCHE = 1'b00;
+                    end
+                    else begin
+                        contatoreManche += 5'b00001;
+                        winPrimo += 5'b00001;
+                        // mossaVincitore = {PRIMO}; visto che la mossa è non giocata non segno la mossa vincitrice senno si va in un loop di pareggi
+                    end
 
                 4'b01 01: 
 
-                    contatoreManche += 5'b00001;
+                    if (vincitore == 1'b1 && mossaVincitore == {SECONDO}) begin
+                        MANCHE = 1'b00;
+                    end
+                    else if (vincitore == 1'b0 && mossaVincitore == {PRIMO}) begin
+                        MANCHE = 1'b00;
+                    end
+                    else begin
+                        contatoreManche += 5'b00001;
+                    end
+                    
                     // controlla eventuali errori per quanto riguarda la mossa del vincitore
 
                 4'b01 10: 
 
-                    contatoreManche += 5'B00001;
-                    winSecondo += 5'b00001;
-                    mossaVincitore = {SECONDO};
+                    if (vincitore == 1'b1 && mossaVincitore == {SECONDO}) begin
+                        MANCHE = 1'b00;
+                    end
+                    else if (vincitore == 1'b0 && mossaVincitore == {PRIMO}) begin
+                        MANCHE = 1'b00;
+                    end
+                    else begin
+                        contatoreManche += 5'B00001;
+                        winSecondo += 5'b00001;
+                        mossaVincitore = {SECONDO};
+                    end
 
                 4'b01 11:
 
-                    contatoreManche += 5'B00001;
-                    winPrimo += 5'b00001;
-                    mossaVincitore = {P};
+                    if (vincitore == 1'b1 && mossaVincitore == {SECONDO}) begin
+                        MANCHE = 1'b00;
+                    end
+                    else if (vincitore == 1'b0 && mossaVincitore == {PRIMO}) begin
+                        MANCHE = 1'b00;
+                    end
+                    else begin
+                        contatoreManche += 5'B00001;
+                        winPrimo += 5'b00001;
+                        mossaVincitore = {PRIMO};
+                    end
 
                 /////////////////////////////////////////
 
                 4'b10 00:
-                
-                    contatoreManche += 5'b00001;
-                    winPrimo += 5'b00001;
-                    mossaVincitore = {PRIMO};
+
+                    if (vincitore == 1'b1 && mossaVincitore == {SECONDO}) begin
+                        MANCHE = 1'b00;
+                    end
+                    else if (vincitore == 1'b0 && mossaVincitore == {PRIMO}) begin
+                        MANCHE = 1'b00;
+                    end
+                    else begin
+                        contatoreManche += 5'b00001;
+                        winPrimo += 5'b00001;
+                        // mossaVincitore = {PRIMO};
+                    end  
 
                 4'b10 01:
 
-                    contatoreManche += 5'B00001;
-                    winPrimo += 5'b00001;
-                    mossaVincitore = {PRIMO};
+                    if (vincitore == 1'b1 && mossaVincitore == {SECONDO}) begin
+                        MANCHE = 1'b00;
+                    end
+                    else if (vincitore == 1'b0 && mossaVincitore == {PRIMO}) begin
+                        MANCHE = 1'b00;
+                    end
+                    else begin
+                        contatoreManche += 5'B00001;
+                        winPrimo += 5'b00001;
+                        mossaVincitore = {PRIMO};
+                    end
 
                 4'b10 10: 
 
-                contatoreManche += 5'b00001;
-                    // controlla eventuali errori per quanto riguarda la mossa del vincitore
+                    if (vincitore == 1'b1 && mossaVincitore == {SECONDO}) begin
+                        MANCHE = 1'b00;
+                    end
+                    else if (vincitore == 1'b0 && mossaVincitore == {PRIMO}) begin
+                        MANCHE = 1'b00;
+                    end
+                    else begin
+                        contatoreManche += 5'b00001;
+                        // controlla eventuali errori per quanto riguarda la mossa del vincitore
+                    end
 
                 4'b10 11:
 
-                    contatoreManche += 5'B00001;
-                    winSecondo += 5'b00001;
-                    mossaVincitore = {SECONDO};
+                    if (vincitore == 1'b1 && mossaVincitore == {SECONDO}) begin
+                        MANCHE = 1'b00;
+                    end
+                    else if (vincitore == 1'b0 && mossaVincitore == {PRIMO}) begin
+                        MANCHE = 1'b00;
+                    end
+                    else begin
+                        contatoreManche += 5'B00001;
+                        winSecondo += 5'b00001;
+                        mossaVincitore = {SECONDO};
+                    end
 
                 /////////////////////////////////////////
 
                 4'b11 00:
 
-                contatoreManche += 5'b00001;
-                    winPrimo += 5'b00001;
-                    mossaVincitore = {PRIMO};
+                    if (vincitore == 1'b1 && mossaVincitore == {SECONDO}) begin
+                        MANCHE = 1'b00;
+                    end
+                    else if (vincitore == 1'b0 && mossaVincitore == {PRIMO}) begin
+                        MANCHE = 1'b00;
+                    end
+                    else begin
+                        contatoreManche += 5'b00001;
+                        winPrimo += 5'b00001;
+                        // mossaVincitore = {PRIMO};
+                    end
 
                 4'b11 01:
 
-                    contatoreManche += 5'B00001;
-                    winSecondo += 5'b00001;
-                    mossaVincitore = {SECONDO};
+                    if (vincitore == 1'b1 && mossaVincitore == {SECONDO}) begin
+                        MANCHE = 1'b00;
+                    end
+                    else if (vincitore == 1'b0 && mossaVincitore == {PRIMO}) begin
+                        MANCHE = 1'b00;
+                    end
+                    else begin
+                        contatoreManche += 5'B00001;
+                        winSecondo += 5'b00001;
+                        mossaVincitore = {SECONDO};
+                    end
 
                 4'b11 10:
-                
-                    contatoreManche += 5'B00001;
-                    winPrimo += 5'b00001;
-                    mossaVincitore = {PRIMO};
+
+                    if (vincitore == 1'b1 && mossaVincitore == {SECONDO}) begin
+                        MANCHE = 1'b00;
+                    end
+                    else if (vincitore == 1'b0 && mossaVincitore == {PRIMO}) begin
+                        MANCHE = 1'b00;
+                    end
+                    else begin
+                        contatoreManche += 5'B00001;
+                        winPrimo += 5'b00001;
+                        mossaVincitore = {PRIMO};
+                    end
 
                 4'b11 11:
 
-                    contatoreManche += 5'b00001;
-                    // controlla eventuali errori per quanto riguarda la mossa del vincitore
+                    if (vincitore == 1'b1 && mossaVincitore == {SECONDO}) begin
+                        MANCHE = 1'b00;
+                    end
+                    else if (vincitore == 1'b0 && mossaVincitore == {PRIMO}) begin
+                        MANCHE = 1'b00;
+                    end
+                    else begin
+                        contatoreManche += 5'b00001;
+                        // controlla eventuali errori per quanto riguarda la mossa del vincitore
+                    end
+                    
                 /////////////////////////////////////////
 
-                default: 
+                default: ///////////////////////////
             endcase
         end
     end
-
 endmodule
